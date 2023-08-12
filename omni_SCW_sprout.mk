ad02b868f1ea3a921b8b7eb3768649457114d2e0
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-08-12 14:04:33 +0530

    Update omni_SCW_sprout.mk

--- omni_SCW_sprout.mk
+++ omni_SCW_sprout.mk
@@ -10,8 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
+# $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 # Inherit from SCW_sprout device
 $(call inherit-product, device/hmd/SCW_sprout/device.mk)
 
