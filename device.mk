f9fc7f8ab460c5a7ae051b996b5a4b8e01591656
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-08-12 14:31:22 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/hmd/SCW_sprout
+LOCAL_PATH := device/hmd/SCWsprout
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
